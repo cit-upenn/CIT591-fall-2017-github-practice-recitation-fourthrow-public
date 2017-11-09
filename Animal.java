74e44b95526157ae5526463c302918e024e09555
1994wyh-WYH <yihanw@seas.upenn.edu>
2017-11-09 16:12:01 -0500

    add Animal.java

---
+++ Animal.java
@@ -0,0 +1,81 @@
+/*
+ * Class defines the animal
+ * Zhao Li is having fun here. ^_^O(°…_°…)O
+ */
+public class Animal {
+
+	private String name;
+	private int age;
+	private int weight;
+	private int height;
+	private String country; 
+	
+	/*
+	 * Constructor of the class
+	 * @param name name of the animal
+	 * @param age age of the animal
+	 * @param weight weight of the animal
+
+	 * @param height height of the animal
+	 * @param country country of origin
+	 */
+	public Animal(String name, int age, int weight, int height, String country) {
+		System.out.println(‚ÄúHello‚Äù);
+	}
+	
+	/**
+	 * Getter for the animal name
+	 * @return name of the animal
+	 */
+	public String getName() {
+		// TODO complete the method
+		return null;
+	}
+	
+	/**
+	 * Setter for the animal name
+	 * @param name name of the animal
+	 */
+	public void setName(String name) {
+		// TODO complete the method, add javadoc comments
+		return;
+	}
+
+	public int getAge() {
+		// TODO complete the method, add javadoc comments
+		return 0;
+	}
+
+	public void setAge(int age) {
+		// TODO complete the method, add javadoc comments
+	}
+
+	public int getWeight() {
+		// TODO complete the method, add javadoc comments
+		return 0;
+	}
+
+	public void setWeight(int weight) {
+		// TODO complete the method, add javadoc comments
+	}
+
+	public int getHeight() {
+		// TODO complete the method, add javadoc comments
+		return 0;
+	}
+
+	public void setHeight(int height) {
+		// TODO complete the method, add javadoc comments
+	}
+
+	public String getCountry() {
+		// TODO complete the method, add javadoc comments
+		return null;
+	}
+
+	public void setCountry(String country) {
+		// TODO complete the method, add javadoc comments
+	}
+	
+	
+}
